6b47a5607622cc3481e8f00fc1f0c950e3ed5e14
clmntmrn <marinclement0@gmail.com>
2024-10-07 19:41:06 -0400

    structure en progres

--- App.js
+++ App.js
@@ -2,37 +2,43 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-//const [data, setData] = useState([]);
-
 export default function App() {
+	const [joke, setJoke] = useState(null);
+
+	const fetchJoke = async () => {
+		try {
+			const response = await fetch(
+				"https://official-joke-api.appspot.com/random_joke"
+			);
+			const data = await response.json();
+			setJoke(data);
+			e;
+		} catch (error) {
+			console.log("Erreur lors de la récupération de la blague :", error);
+		}
+	};
+
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+			<Text style={styles.title}>Blague aléatoire</Text>
 			<View style={{ width: "40%" }}>
 				<TouchableOpacity
-					onPress={async () => {
-						try {
-							const response = await fetch(
-								"https://official-joke-api.appspot.com/random_joke"
-							);
-							const data = await response.json(); // On convertit la réponse en JSON
-							console.log(data);
-						} catch (error) {
-							console.log(
-								"Erreur lors de la récupération de la blague :",
-								error
-							);
-						}
-					}}
+					onPress={fetchJoke}
 					style={{
 						...styles.button,
 						width: "100%",
 						alignItems: "center",
 						justifyContent: "center",
 					}}>
-					<Text>click-me</Text>
+					<Text>Click-me</Text>
 				</TouchableOpacity>
 			</View>
+			{joke && (
+				<View style={styles.jokeContainer}>
+					<Text style={styles.jokeSetup}>{joke.setup}</Text>
+					<Text style={styles.jokePunchline}>{joke.punchline}</Text>
+				</View>
+			)}
 		</View>
 	);
 }
@@ -45,10 +51,29 @@ const styles = StyleSheet.create({
 		height: "100%",
 		justifyContent: "center",
 	},
+	title: {
+		fontSize: 24,
+		marginBottom: 20,
+	},
 	button: {
 		height: 40,
 		borderRadius: 10,
-		width: "40%",
 		backgroundColor: "#3333",
+		marginBottom: 20,
+	},
+	jokeContainer: {
+		marginTop: 20,
+		padding: 10,
+		borderColor: "#ccc",
+		borderWidth: 1,
+		borderRadius: 5,
+		width: "90%",
+	},
+	jokeSetup: {
+		fontWeight: "bold",
+	},
+	jokePunchline: {
+		marginTop: 5,
+		fontStyle: "italic",
 	},
 });
